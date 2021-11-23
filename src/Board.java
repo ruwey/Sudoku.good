4953c1c50535d589d303546641a4ea03d80c4206
gd <gd@ruwey.com>
2021-11-23 12:30:55 -0500

    possible work!!!! (pogular champion)

--- src/Board.java
+++ src/Board.java
@@ -12,8 +12,7 @@ public class Board {
     public Board() {
         Random r = new Random();
         for (int row = 0; row < correct_board.length; row++) {
-            int[] nums = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-            new_row(0, 0, nums, r);
+            new_cell(0, 0, r);
         }
     }
 
@@ -39,6 +38,12 @@ public class Board {
                 return false;
         return true;
     }
+    public boolean check_row(int row, int num) {
+        for (int i = 0; i < 9; i++)
+            if (this.correct_board[row][i] == num)
+                return false;
+        return true;
+    }
     public boolean check_box(int row, int column, int num) {
         for (int i = (row/3) * 3; i < (row/3+1)*3; i++)
             for (int j = (column/3) * 3; j < (column/3+1)*3; j++) {
@@ -48,26 +53,27 @@ public class Board {
             }
         return true;
     }
-    private boolean new_row(int row, int column, Random r) {
-        int[] pos = (int[]) Collections.shuffle();
+    private boolean new_cell(int row, int column, Random r) {
+        List<Integer> pos = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
+        Collections.shuffle(pos);
+        System.out.println(row + ", " + column);
+        System.out.println(this);
         for (int num: pos) {
-            System.out.println(row + ", " + column);
-            System.out.println(this);
-            if (check_column(column, num) && check_box(row, column, num)) {
+            if (check_column(column, num) && check_box(row, column, num) && check_row(row, num)) {
                 correct_board[row][column] = num;
                 if (row == 8 && column == 8)
                     return true;
                 else if (column == 8) {
                     row++;
                     column = 0;
-                    pos = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
                 }
                 else
                     column++;
-                boolean ret = new_row(row, column, pos, r);
+                boolean ret = new_cell(row, column, r);
                 if (ret) return true;
             }
         }
+        correct_board[row][column] = 0;
         System.out.println("Backtracking");
         return false;
     }
